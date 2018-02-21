708e812049c2343b0899c673e5276ba116f7c1f2
Kyle Isom <kyle@imap.cc>
2018-02-21 10:50:23 -0800

    Start kf (kyle's forth)
    Here we go with another pass at writing a Forth.

---
+++ misc/kf/io.h
@@ -0,0 +1,10 @@
+#ifndef __KF_IO_H__
+#define __KF_IO_H__
+
+// TODO(kyle): make this selectable by architecture.
+
+class IO {
+
+};
+
+#endif // __KF_IO_H__
---
+++ misc/kf/kf.cc
@@ -0,0 +1,8 @@
+#include "io.h"
+
+
+int
+main(void)
+{
+	return 0;
+}
