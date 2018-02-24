4dae55e02eb5c77d2a4ef993bb1f331ecaa591fd
Kyle Isom <kyle@imap.cc>
2018-02-23 21:15:33 -0800

    misc/kforth: Iron out numeric issues.

--- io.cc
+++ io.cc
@@ -3,25 +3,32 @@
 
 #include <string.h>
 
+static constexpr size_t	nbuflen = 11;
+
 void
 write_num(IO &interface, KF_INT n)
 {
 
-	// TODO(kyle): make the size of the buffer depend on the size of 
+	// TODO(kyle): make the size of the buffer depend on the size of
 	// KF_INT.
-	char buf[10];
-	uint8_t i = 10;
+	char buf[nbuflen];
+	uint8_t i = nbuflen;
 	memset(buf, 0, i);
+	bool neg = n < 0;
+
 	if (n < 0) {
 		interface.wrch('-');
-		n *= -1;
+		n = ~n;
 	}
 
 	while (n != 0) {
 		char ch = (n % 10) + '0';
-		buf[i--] = ch;
+		if (neg && (i == nbuflen)) ch++;
+		buf[i-1] = ch;
+		i--;
 		n /= 10;
 	}
 
-	interface.wrbuf(buf+i, 11-i);
+	uint8_t buflen = nbuflen - i % nbuflen;
+	interface.wrbuf(buf+i, buflen);
 }
