25bfca067c7c1dcbe15c5a7189a09bbb1ed7808b
Kyle Isom <kyle@imap.cc>
2018-05-06 20:21:35 -0700

    django: finish tutorial #3

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+
+from . import models
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions = models.Question.objects.order_by('-pub_date')[:5]
+    ctx = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', ctx)
+
+def detail(request, question_id):
+    q = get_object_or_404(models.Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': q})
+
+def results(request, question_id):
+    response = "Tally for \"{}\":".format(question_id)
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on {}".format(question_id))
+
