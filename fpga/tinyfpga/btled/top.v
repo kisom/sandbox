95827da3e100613749630b16feb93b6c61e2eb3b
Kyle Isom <kyle@imap.cc>
2018-12-29 09:55:32 -0800

    TinyFPGA: add button-toggled LED.

---
+++ fpga/tinyfpga/btled/top.v
@@ -0,0 +1,22 @@
+/// bled
+/// Button-toggled LED
+///
+/// This toggles the LED using a pushbutton on pin 6. The pushbutton
+/// has one side connected to Vcc, and the other side to both pin 6 and
+/// a 10K Ω resistor (because, for some reason, that's what I have on
+/// hand).
+module top (
+	input	PIN_6,
+	input	CLK,
+	output	LED
+);
+
+	reg	state = 0;
+	reg	pressed = 0;
+
+	assign LED = state;
+	always @(negedge PIN_6)
+		state <= !state;
+
+
+endmodule
