8450ce87530a9bd997a234a7f5bf11e274e473b3
Kyle Isom <kyle@imap.cc>
2019-04-22 07:01:00 -0700

    Updating PE.

--- pe/shop.erl
+++ pe/shop.erl
@@ -1,4 +1,14 @@
 -module(shop).
--export([cost/1]).
+-export([cost/1, total/1]).
 
+%% ShoppingList = [{oranges,4},{newspaper,1},{apples,10},{pears,6},{milk,3}]
+cost (oranges) -> 5;
+cost (newspaper) -> 8;
+cost (apples) -> 2;
+cost (pears) -> 9;
+cost (milk) -> 7.
 
+total([]) ->
+    0;
+total([{Item, Count}|Rest]) ->
+    cost(Item) * Count + total(Rest).
